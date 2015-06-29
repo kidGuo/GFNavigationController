8026d11891bd0c54f258fa568c2440e8a81f77fa
guofeng <guofeng@belo-inc.com>
2015-06-29 09:51:47 +0800

    fix bugs

---
+++ GFNavigationControllerSample/GFNavigationControllerClass/UIViewController+GFNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+GFNavigationController.h
+//  GFNavigationControllerSample
+//
+//  Created by MeTao on 15/6/24.
+//  Copyright (c) 2015年 kid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (GFNavigationController)
+
+@end
